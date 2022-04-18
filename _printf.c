613cb09810a9daecde6f42a6b3365bcaee0d44b7
ziclude <cindyzickziemi@gmail.com>
2022-04-18 02:15:32 -0700

    betty correction

--- _printf.c
+++ _printf.c
@@ -8,39 +8,39 @@
  * @list: list of arguments to print
  * Return: numb of char to be printed
  */
+
 int print_op(const char *format, fmt_t *print_arr, va_list list)
 {
-	char a;
-	int count = 0, b = 0, c = 0;
-
-	a = format[b];
-	while (a != '\0')
-	{
-		if (a == '%')
-		{
-			c = 0;
-			b++;
-			a = format[b];
-			while (print_arr[c].type != NULL &&
-			       a != *(print_arr[c].type))
-				c++;
-			if (print_arr[c].type != NULL)
-				count = count + print_arr[c].f(list);
-			else
-			{
-				if (a == '\0')
-					return (-1);
-				if (a != '%')
-					count += _putchar('%');
-				count += _putchar(a);
-			}
-		}
-		else
-			count += _putchar(a);
-		b++;
-		a = format[b];
-	}
-	return (count);
+char a;
+int count = 0, b = 0, c = 0;
+a = format[b];
+while (a != '\0')
+{
+if (a == '%')
+{
+c = 0;
+b++;
+a = format[b];
+while (print_arr[c].type != NULL &&
+a != *(print_arr[c].type))
+c++;
+if (print_arr[c].type != NULL)
+count = count + print_arr[c].f(list);
+else
+{
+if (a == '\0')
+return (-1);
+if (a != '%')
+count += _putchar('%');
+count += _putchar(a);
+}
+}
+else
+count += _putchar(a);
+b++;
+a = format[b];
+}
+return (count);
 }
 
 /**
@@ -48,29 +48,28 @@ int print_op(const char *format, fmt_t *print_arr, va_list list)
  * @format: string being passed
  * Return: char to be printed
  */
+
 int _printf(const char *format, ...)
 {
-	va_list list;
-	int a = 0;
-
-	fmt_t ops[] = {
-		{"c", ch},
-		{"s", str},
-		{"d", _int},
-		{"b", _bin},
-		{"i", _int},
-		{"u", _ui},
-		{"o", _oct},
-		{"x", _hex_l},
-		{"X", _hex_u},
-		{"R", _rot13},
-		{NULL, NULL}
-	};
-
-	if (format == NULL)
-		return (-1);
-	va_start(list, format);
-	a = print_op(format, ops, list);
-	va_end(list);
-	return (a);
+va_list list;
+int a = 0;
+fmt_t ops[] = {
+{"c", ch},
+{"s", str},
+{"d", _int},
+{"b", _bin},
+{"i", _int},
+{"u", _ui},
+{"o", _oct},
+{"x", _hex_l},
+{"X", _hex_u},
+{"R", _rot13},
+{NULL, NULL}
+};
+if (format == NULL)
+return (-1);
+va_start(list, format);
+a = print_op(format, ops, list);
+va_end(list);
+return (a);
 }
